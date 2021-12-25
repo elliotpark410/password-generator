7e3762095db3f81577c3c58497c383099c4d0116
Elliot Park <elliotpark410@gmail.com>
2021-12-24 21:54:26 -0800

    create function that prompts for password character length

--- Develop/script.js
+++ Develop/script.js
@@ -7,18 +7,52 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Create password crtieria prompt for when user clicks button 
-function writePassword() {
-  let passwordCriteria = prompt("Please chooose your password criteria");
 
-  if (writePassword != null) {
-    return;
-  }
+
+
+
+
+// Create funtion for passwordLength prompt
+function passwordLength() {
+
+  // initial prompt asking user to enter number of characters
+  let passwordLengthInput = prompt("Please choose how many characters you would like your password to contain");
+
+     // if user hits cancel on prompt, then it cancels functions
+     if (passwordLengthInput === null) {
+      return;
+    }
+
+    // if password length input > 128, then user gets alert
+    else if (passwordLengthInput > 128) {
+      alert("Password must be less than 128 characters")
+    }
+
+     // if password length input <= 7, then user gets alert
+    else if (passwordLengthInput <= 7) {
+      alert("Password must be greater than or equal to 8 characters")
+    }
+
+    // if password length is in 
+    else {
+      generatePassword();
+    }
 
 }
+
+// NEED TO DELETE JUST FOR TESTING PURPOSES
+passwordLength();
+
+
+
+
+
+
+
+
+
