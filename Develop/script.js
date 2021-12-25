c8a4b3e18d69c1833b998c519286ca6fde17acbd
Elliot Park <elliotpark410@gmail.com>
2021-12-24 23:49:47 -0800

    add confirm window for special characters

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
@@ -23,30 +24,48 @@ function passwordLength() {
   // initial prompt asking user to enter number of characters
   let passwordLengthInput = prompt("Please choose how many characters you would like your password to contain");
 
-     // if user hits cancel on prompt, then it cancels functions
-     if (passwordLengthInput === null) {
-      return;
-    }
+    // if user hits cancel on prompt, then it cancels functions
+  if (passwordLengthInput === null) {
+    return;
+  }
 
-    // if password length input > 128, then user gets alert
-    else if (passwordLengthInput > 128) {
-      alert("Password must be less than 128 characters")
-    }
+  // if password length input > 128, then user gets alert
+  else if (passwordLengthInput > 128) {
+    alert("Password must be less than 128 characters");
+  }
 
-     // if password length input <= 7, then user gets alert
-    else if (passwordLengthInput <= 7) {
-      alert("Password must be greater than or equal to 8 characters")
-    }
+    // if password length input <= 7, then user gets alert
+  else if (passwordLengthInput <= 7) {
+    alert("Password must be greater than or equal to 8 characters");
+  }
 
-    // if password length is in 
-    else {
-      generatePassword();
-    }
+  // if password length is in. NOT SURE IF THIS IS CORRECT
+  else {
+    generatePassword(); // NEED GENERATE PASSWORD TO INCORPORATE PASSWORD LENGTH INPUT
+  }
 
 }
 
+
+
+// Create function for specialCharacters confirmation
+function specialCharacters() {
+  let specialCharactersConfirmation = "Click OK to include special characters";
+
+  // If user hits Ok, then password will incorporate special characters
+  if (confirm(specialCharactersConfirmation) == true) {
+     // ADD CODE. NEED TO GENERATE PASSWORD WITH SPECIAL CHARACTERS
+  
+
+  // If user hits Cancel, then password will not incorporate special characters
+  } else {
+        // ADD CODE. NEED TO GERENATE PASSWORD WITHOUT SPECIAL CHARACTERS
+
+  }
+  
+}
 // NEED TO DELETE JUST FOR TESTING PURPOSES
-passwordLength();
+specialCharacters();
 
 
 
