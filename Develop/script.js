c203a6aff3c4c5117988c9536ae7e602c3060f26
Elliot Park <elliotpark410@gmail.com>
2021-12-27 02:43:47 -0800

    create generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,181 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create variables for potential characters and placeholders for password length and whether to include characters in the password
+var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
+var numericCharacters = '0123456789';
+var specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~"';
+
+
+var passwordLength;
+var includeLowercase;
+var includeUppercase;
+var includeNumeric;
+var includeSpecialCharacter;
+
+
+// Create function prompt for password length prompt
+function passwordLengthPrompt() {
+  var passwordLengthInput = prompt("Please choose how many characters you would like your password to contain between 8-128 characters");
+
+  if (passwordLengthInput > 128) {
+    alert("Password must be less than 128 characters");
+    passwordLengthPrompt();
+  } else if (passwordLengthInput < 8) {
+    alert("Password must be more than 7 characters");
+    passwordLengthPrompt();
+  } else if (isNaN(passwordLengthInput)) {
+    alert("Password must be a number between 8 and 128 characters");
+    passwordLengthPrompt();
+  } else {
+    passwordLength = passwordLengthInput;
+  }
+  return passwordLength;
+}
+// passwordLengthPrompt();
+console.log("passwordLength: " + passwordLength);
+
+
+
+// Create confirmation for lowercase
+function lowercaseConfirmation() {
+  var lowercaseConfirmationInput = confirm("Click OK to include lowercase characters");
+
+  if (lowercaseConfirmationInput) {
+    console.log("user hit ok to include lowercase");
+    includeLowercase = true;
+    return includeLowercase;
+
+  } else {
+    console.log("user hit cancel to not include lowercase");
+    includeLowercase = false;
+    return includeLowercase;
+  }
+  return includeLowercase;
+}
+// lowercaseConfirmation();
+
+
+
+// Create confirmation for uppercase
+function uppercaseConfirmation() {
+  var uppercaseConfirmationInput = confirm("Click OK to include uppercase characters");
+
+  if (uppercaseConfirmationInput) {
+    console.log("user hit ok to include uppercase");
+    includeUppercase = true;
+    return includeUppercase;
+
+  } else {
+    console.log("user hit cancel to not include uppercase");
+    includeUppercase = false;
+    return includeUppercase;
+  }
+  return includeUppercase;
+}
+// uppercaseConfirmation();
+
+
+
+// Create confirmation for numeric
+function numericConfirmation() {
+  var numericConfirmationInput = confirm("Click OK to include numeric characters");
+
+  if (numericConfirmationInput) {
+    console.log("user hit ok to include numeric characters");
+    includeNumeric = true;
+    return includeNumeric;
+
+  } else {
+    console.log("user hit cancel to not include numeric characters");
+    includeNumeric = false;
+    return includeNumeric;
+  }
+  return includeNumeric;
+}
+// numericConfirmation();
+
+
+
+// Create confirmation for specialCharacters
+function specialCharactersConfirmation() {
+  var specialCharactersConfirmationInput = confirm("Click OK to include special characters");
+
+  if (specialCharactersConfirmationInput) {
+    console.log("user hit ok to include special characters");
+    includeSpecialCharacter = true;
+    return includeSpecialCharacter;
+
+  } else {
+    console.log("user hit cancel to not include special characters");
+    includeSpecialCharacter = false;
+    return includeSpecialCharacter;
+  }
+  return includeSpecialCharacter;
+}
+// specialCharactersConfirmation();
+
+// Create alert window to notify user that they have to choose one character type to include in password
+function noCharactersIncluded() {
+  if (includeLowercase === false &&
+    includeUppercase === false &&
+    includeNumeric === false &&
+    includeSpecialCharacter === false) {
+    alert("Please press OK to at least one character type to include in your password");
+  }
+} 
+
+
+
+
+// Create function to generate password using random number 
+function generatePassword() {
+
+  // Run all functions created above
+  passwordLengthPrompt();
+  lowercaseConfirmation();
+  uppercaseConfirmation();
+  numericConfirmation();
+  specialCharactersConfirmation();
+  noCharactersIncluded();
+
+  var randomPassword = "";
+  var includedCharacters = lowercaseCharacters;
+
+  if (includeUppercase && includeNumeric && includeSpecialCharacter) {
+    includedCharacters += uppercaseCharacters + numericCharacters + specialCharacters;
+  
+  } else if (includeUppercase && includeSpecialCharacter) {
+    includedCharacters += uppercaseCharacters + specialCharacters;
+
+  } else if (includeUppercase && includeNumeric) {
+    includedCharacters += uppercaseCharacters + numericCharacters;
+
+  } else if (includeNumeric && includeSpecialCharacter) {
+    includedCharacters += numericCharacters + specialCharacters;
+  
+  } else if (includeUppercase) {
+    includedCharacters += uppercaseCharacters;
+  
+  } else if (includeNumeric) {
+    includedCharacters += numericCharacters;
+
+  } else if (includeSpecialCharacter) {
+    includedCharacters += specialCharacters;
+
+  } else {
+    includedCharacters === lowercaseCharacters;
+  }
+
+  // generate randompassword with for loop and char at method - returns the character at a specified position
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += includedCharacters.charAt(Math.floor(Math.random() * includedCharacters.length));
+  }
+  return randomPassword;
+} 
+  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,164 +183,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  // Create funtion for passwordLength prompt
-  function passwordLengthFunction() {
-    // initial prompt asking user to enter number of characters
-    let passwordLengthInput = prompt("Please choose how many characters you would like your password to contain");
-    
-    // if user hits cancel on prompt, then it cancels functions
-    if (passwordLengthInput === null) {
-      console.log("user hit cancel in password length prompt");
-      return; // PROBLEM: CONTINUES TO NEXT PROMPT  
-    }
-
-    // if password length input > 128, then user gets alert
-    else if (passwordLengthInput > 128) {
-      alert("Password must be less than 128 characters");
-      return; // PROBLEM: CONTINUES TO NEXT PROMPT AND DOESN'T RERUN FUNCTION
-    }
-
-    // if password length input <= 7, then user gets alert
-    else if (passwordLengthInput <=7 ) {
-      alert("Password must be greater than or equal to 8 characters");
-      return; // PROBLEM: CONTINUES TO NEXT PROMPT AND DOESN'T RERUN FUNCTION
-    }
-
-    else if (passwordLengthInput > 7 && passwordLengthInput < 129) {
-      passwordLength[0] = passwordLengthInput;
-      console.log("passwordLength: " + passwordLength);  
-    }
-  }
-  passwordLengthFunction();
-
-
-  // Create function for lowercase characters confirmation
-  function lowercaseFunction() {
-    let lowercaseConfirmation = "Click OK to include lowercase characters";
-
-    // If user hits Ok, then password will include lowercase characters
-    if (confirm(lowercaseConfirmation) == true) {
-      console.log("user hit ok to include lowercase");
-      includeLowercase = true;
-    // If user hits Cancel, then password will not include lowercase characters
-    } else {
-        console.log("user hit cancel to not include lowercase");
-        includeLowercase = false;
-    }
-  } 
-  lowercaseFunction();
-   
-  
-  // Create function for uppercase characters confirmation
-  function uppercaseFunction() {
-    let uppercaseConfirmation = "Click OK to include uppercase characters";
-
-    // If user hits Ok, then password will include uppercase characters
-    if (confirm(uppercaseConfirmation) == true) {
-      console.log("user hit ok to include uppercase");
-      includeUppercase = true;
-    // If user hits Cancel, then password will not include uppercase characters
-    } else {
-        console.log("user hit cancel to not include uppercase");
-        includeUppercase = false;
-    }
-  }
-  uppercaseFunction();
-
-
-  // Create function for numeric characters confirmation
-  function numericFunction() {
-    let numericConfirmation = "Click OK to include numeric characters";
-
-    // If user hits Ok, then password will include numeric characters
-    if (confirm(numericConfirmation) == true) {
-      console.log("user hit ok to include numeric characters");
-      includeNumeric = true;
-    // If user hits Cancel, then password will not include numeric characters
-    } else {
-      console.log("user hit cancel to not include numeric characters");
-      includeNumeric = false;
-    }
-  }
-  numericFunction();
-
-
-  // Create function for special characters confirmation
-  function specialCharacterFunction() {
-    let specialCharacterConfirmation = "Click OK to include special characters";
-
-    // If user hits Ok, then password will include special characters
-    if (confirm(specialCharacterConfirmation) == true) {
-      console.log("user hit ok to include special characters");
-      includeSpecialCharacter = true;
-
-    // If user hits Cancel, then password will not include special characters
-    } else {
-        console.log("user hit cancel to not include special characters");
-        includeSpecialCharacter = false;
-    }
-  }
-  specialCharacterFunction();
-
-
-
-  // Create function to generate random lower case
-  function getRandomLowercaseCharacter() {
-    let lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
-    return lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
-  }
-  console.log(getRandomLowercaseCharacter());
-
-
-  // Create function to generate random upper case
-  function getRandomUppercaseCharacter() {
-    let uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
-    return uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
-  }
-  console.log(getRandomUppercaseCharacter());
-
-
-  // Create function to generate random number character
-  function getRandomNumericCharacter() {
-    let numericCharacters = '0123456789'
-    return numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-  }
-  console.log(getRandomNumericCharacter());
-
-
-  // Create function to generate random special character
-  function getRandomSpecialCharacter() {
-    let specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"';
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  }
-  console.log(getRandomSpecialCharacter());
-
-
-
-  // Create function so generate password function knows password length and if it incudes lowercase, uppercase, numeric, and special characters
-  function generatePassword() {
-
-  }
-  generatePassword();
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
